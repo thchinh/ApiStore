d420e9d8cf15ade13fd236e7328b53f5bb568bea
Hữu Chinh <khleson79929@gmail.com>
2024-04-22 23:02:56 +0700

    update url

--- ApiStore/Program.cs
+++ ApiStore/Program.cs
@@ -31,7 +31,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+
 
 app.UseAuthorization();
 
